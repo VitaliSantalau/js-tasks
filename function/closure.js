a1fe9c663231af917ae11c6ce088d1016a9933eb
VitaliSantalau <v.santalau@gmail.com>
2021-09-15 18:00:50 +0300

    feat: exp

--- function/closure.js
+++ function/closure.js
@@ -1,28 +1,83 @@
-function formatDuration (seconds) {
-  if(seconds === 0) return 'now';
-  
-  const outSec = seconds % 60;
-  const getMin = (seconds - outSec) / 60;
-  const outMin = getMin > 60 ? getMin % 60 : getMin;
-  const getHour =  (getMin - outMin) / 60;
-  const outHour = getHour > 24 ? getHour % 60 : getHour;
-  const getDay = (getHour - outHour) / 365;
-  const outDay = getDay > 365 ? getDay % 365 : getDay;
-  const getYear = (getDay - outDay) / 365;
+// function formatDuration (seconds) {
+//   if(seconds === 0) return 'now';
 
-console.log(outHour, outMin, outSec)
+//   const getSec = seconds % 60;
+//   const getMin = Math.floor(seconds % 3600 / 60);
+//   const getHour = Math.floor(seconds % (3600*24) / 3600);
+//   const getDay = Math.floor(seconds / (3600*24)) % 365;
+//   const getYear = Math.floor(seconds / (3600*24*365));
 
-  function getElem(type, multip) {}
+//   function output(num, type) {
+//     return num > 1 ? `${num} ${type}s` : `1 ${type}`;
+//   }
 
+//   const sec = getSec > 0 ? output(getSec, 'second') : 0;
+//   const min = getMin > 0 ? output(getMin, 'minute') : 0;
+//   const hour = getHour > 0 ? output(getHour, 'hour') : 0;
+//   const day = getDay > 0 ? output(getDay, 'day') : 0;
+//   const year = getYear > 0 ? output(getYear, 'year') : 0;
 
-  function output(num, type) {
-    return num > 1 
-      ? `${num} ${type}s`
-      : `1 ${type}`;
+//   const arr = [year, day, hour, min, sec].filter(elem => elem !== 0);
+//   const getStr = arr.length > 1 ? arr.join(', ') : arr[0];
+//   const index = getStr.lastIndexOf(',');
+
+//   return  index >=0 ? `${getStr.slice(0, index)} and${getStr.slice(index+1)}` : getStr;
+// }
+
+// function myLanguages(results) {
+//   return Object.entries(results).filter(([, value]) => value >= 60).sort(([, valueA], [, valueB]) => {
+//     return valueA >= valueB ? -1 : 1;
+//   }).reduce((acc, [key, ]) =>  {
+//     acc.push(key);
+//     return acc;
+//   }, []);
+// }
+
+// var runLengthEncoding = function(str){
+//   if(str.length === 0) return [];
+
+//   const arr = [];
+//   const strArr = str.split('');
+//   let subArr = [1, strArr[0]];
+
+//   for(let i = 1; i < strArr.length; i++) {
+//     if(strArr[i] === strArr[i-1]) {
+//       subArr[0] += 1;
+//     } else {
+//       arr.push(subArr);
+//       subArr = [1, strArr[i]]
+//     }
+//   }
+//   arr.push(subArr);
+//   return arr;
+
+// }
+
+var WordDictionary = function () {
+  this.dictionary = new Set();
+};
+
+WordDictionary.prototype.addWord = function (word) {
+   this.dictionary.add(word)
+};
+
+WordDictionary.prototype.search = function (word) {
+  const arrWord = word.split('');
+  const regexp = /b/;
+  // return this.dictionary.has(regexp)
+  for(let value of this.dictionary) {
+    if(value.match(regexp)) return true;
+    return false;
   }
+};
 
-  
-}
+var wd = new WordDictionary();
+  wd.addWord('a');
+  wd.addWord('at');
+  wd.addWord('ate');
+  wd.addWord('ear');
+
+console.log(wd.search('a'));
 
 
-console.log(formatDuration(62));
+// wd.search('a.e')
